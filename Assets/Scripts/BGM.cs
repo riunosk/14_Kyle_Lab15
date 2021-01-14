f6182794b34407edba51cf3c8bc30713498a48d9
riunosk <riunosk@outlook.com>
2021-01-14 19:49:57 +0800

    school

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public AudioClip[] bgmArray;
+
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float audioVolume = audioSource.volume;
+        if (Input.GetKeyDown(KeyCode.Tab))
+		{
+            audioSource.Stop();
+            int rand = Random.Range(0, bgmArray.Length);
+            audioSource.PlayOneShot(bgmArray[rand]);
+		}
+        if (Input.GetKey(KeyCode.UpArrow))
+		{
+            audioSource.volume = audioVolume + Time.deltaTime;
+		}
+        if (Input.GetKey(KeyCode.DownArrow))
+		{
+            audioSource.volume = audioVolume - Time.deltaTime;
+		}
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public GameObject jumpText;
+    public AudioClip[] AudioClipArray;
+
+    private int JumpCounter;
+    private Rigidbody rb;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+            JumpCounter++;
+            jumpText.GetComponent<Text>().text = "Jump: " + JumpCounter;
+            rb.AddForce(new Vector3(0, 250, 0));
+            int rand = Random.Range(0, AudioClipArray.Length);
+            audioSource.PlayOneShot(AudioClipArray[rand]);
+		}
+    }
+}
